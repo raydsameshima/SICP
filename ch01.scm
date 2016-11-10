40abceff7d729e0b35b5c8ecb60630df1bee6daa
raydsameshima <rsameshima@citytech.cuny.edu>
2016-11-10 00:42:36 -0500

    accurman function

--- ch01.scm
+++ ch01.scm
@@ -27,9 +27,10 @@
 (define (helper g x)
   (abs (/ (- (square g) x) x)))
 
+; 1.1.8
 (define (another-sqrt x)
   (define (good? g x)
-    (< (abs (- (square g) x)) 0.001))
+    (< (abs (/ (- (square g) x) x)) 0.001))
   (define (improve g x) (average g (/ x g)))
   (define (another-sqrt-iter g x)
     (if (good? g x)
@@ -37,6 +38,25 @@
         (another-sqrt-iter (improve g x) x)))
   (another-sqrt-iter 1.0 x))
   
+; 1.2.1
+(define (rfactorial n)
+  (if (or (= n 1) (< n 1))
+      1
+      (* n (rfactorial (- n 1)))))
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+        product
+        (iter (* counter product)
+              (+ counter 1))))
+  (iter 1 1))
+
+; excercise 1.10
+(define (accurman x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (accurman (- x 1) (accurman x (- y 1))))))
 
 
 
