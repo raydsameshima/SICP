69df7722a9b0b7267a356204c9269c11b1fce748
raydsameshima <rsameshima@citytech.cuny.edu>
2016-11-10 23:51:59 -0500

    Ackermann function, with lhs

--- Accurman.lhs
+++
@@ -1,9 +0,0 @@
-Accurman.lhs
-
-> module Accurman where
-
-> -- accurman :: Int -> Int -> Int
-> accurman _ 0 = 0
-> accurman 0 y = 2*y
-> accurman _ 1 = 2
-> accurman x y = accurman (x-1) (accurman x (y-1))
--- ch01.scm
+++ ch01.scm
@@ -1,9 +1,28 @@
 ; ch01.scm
 ; To load this file, type (load "ch01.scm")
 ;
-; SICP chapter 01
-;
-; 1.1.7 sqrt with Newton method
+; SICP 
+; chapter 01
+; Building Abstractions with Procedures
+; 1.1 The Elements of Programming
+; 1.1.1 Expressions
+; 1.1.2 Naming and the Environment
+; 1.1.3 Evaluating Combinations
+; 1.1.4 Compound Procedures
+; 1.1.5 The Substitution Model for Procedure Application
+; 1.1.6 Conditional Expressions and Predicates
+; Exercise 1.3
+(define (sum-of-largers a b c)
+  (define (larger-one a b)
+    (if (> a b) a b))
+  (define (smaller-one a b)
+    (if (< a b) a b))
+  (+ (larger-one a b) (larger-one c (smaller-one a b))))
+
+(define (sum-of-square-of-largers a b c)
+  (sum-of-largers (* a a) (* b b) (* c c)))
+  
+; 1.1.7 Example: Square Roots by Newton's Method
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
       guess
@@ -27,7 +46,7 @@
 (define (helper g x)
   (abs (/ (- (square g) x) x)))
 
-; 1.1.8
+; 1.1.8 Procedures as Black-Box Abstractions
 (define (another-sqrt x)
   (define (good? g x)
     (< (abs (/ (- (square g) x) x)) 0.001))
@@ -38,11 +57,14 @@
         (another-sqrt-iter (improve g x) x)))
   (another-sqrt-iter 1.0 x))
   
-; 1.2.1
+; 1.2 Procedures and the Processes They Generate
+; 1.2.1 Linear Recursion and Iteration
+; The factorial function of recursion version.
 (define (rfactorial n)
   (if (or (= n 1) (< n 1))
       1
       (* n (rfactorial (- n 1)))))
+; The tail-recursion version.
 (define (factorial n)
   (define (iter product counter)
     (if (> counter n)
@@ -50,10 +72,12 @@
         (iter (* counter product)
               (+ counter 1))))
   (iter 1 1))
+
 ; exercise 1.9
-(define (r-plus a b)
+(define (rplus a b)
   (if (= a 0) b
-              (inc (r-plus (dec a) b))))
+              (inc (rplus (dec a) b))))
+; The tail-recursive version.
 (define (plus a b)
   (if (= a 0) b
               (plus (dec a) (inc b))))
@@ -61,11 +85,12 @@
 (define (dec a) (- a 1))
 
 ; excercise 1.10
-(define (accurman x y)
+; Ackermann's function.
+(define (ack x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
         ((= y 1) 2)
-        (else (accurman (- x 1) (accurman x (- y 1))))))
+        (else (ack (- x 1) (ack x (- y 1))))))
 
 ; 1.2.2
 (define (t_fib n)
