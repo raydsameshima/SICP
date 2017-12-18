e89491fe8b7be472ff012fe0c2e887fef36071df
Ray D. Sameshima <rsameshima@citytech.cuny.edu>
2017-12-17 23:58:45 -0500

    use let* for multiple local variables

--- another/06local.scm
+++ another/06local.scm
@@ -15,14 +15,26 @@
                (list (+ e f) (- e f)))))))))
 
 ; The follwoing does not work.
+; Since the scope is in the body (let binds body)
 ; (define (throw0 v deg)
-;   (let ((pi (* 4 (atan 1.0)))
+;   (let ((pi (* 4 (atan 1.0)))     ; binds
 ;         (rad (* pi (/ deg 180)))
 ;         (vx (* v (cos rad)))
 ;         (vy (* v (sin rad)))
 ;         (t (/ (* 2 vy) 9.81)))
-;        (* vx t)))
+;     (* vx t)))                    ; body
 
+; So we can use let*: 
+(define (throw- v a)
+  (let* ((pi (* 4 (atan 1.0)))
+         (rad (* pi (/ a 180)))
+         (vx (* v (cos rad)))
+         (vy (* v (sin rad)))
+         (t (/ (* 2 vy) 9.81)))
+    (* vx t)))
+
+;
 (define (throw v a)
   (let ((r (/ (* 4 a (atan 1.0)) 180)))
-       (/ (* 2 v v (cos r) (sin r)) 9.8)))
+       (/ (* 2 v v (cos r) (sin r)) 9.81)))
+
