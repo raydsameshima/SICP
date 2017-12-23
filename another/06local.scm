8a0f43396e94e313025311cd57b2baab7edb1a6b
Ray D. Sameshima <rsameshima@citytech.cuny.edu>
2017-12-22 23:34:06 -0500

    let* can refer the inner binds

--- another/06local.scm
+++ another/06local.scm
@@ -1,18 +1,18 @@
 ; 06 local variable
+; http://www.shido.info/lisp/scheme6.html
+;
 ; let
-
-; 
 (define (quad-eq a b c)
   (if (zero? a)
-     ('error)
-     (let ((d (- (* b b) (* 4 a c)))) 
-       (if (negative? d)
-         ('())
-         (let ((e (/ b a -2)))
-           (if (zero? d)
-             (list e)
-             (let ((f (/ (sqrt d) a 2)))
-               (list (+ e f) (- e f)))))))))
+      ('error)
+      (let ((d (- (* b b) (* 4 a c)))) ; determinant
+        (if (negative? d)
+          ('()) ; no real root
+          (let ((e (/ b a -2))) ; e= b/a/(-2)= -2b/a
+            (if (zero? d)
+              (list e)
+              (let ((f (/ (sqrt d) a 2)))
+                (list (+ e f) (- e f)))))))))
 
 ; The follwoing does not work.
 ; Since the scope is in the body (let binds body)
@@ -24,16 +24,16 @@
 ;         (t (/ (* 2 vy) 9.81)))
 ;     (* vx t)))                    ; body
 
-; So we can use let*: 
-(define (throw- v a)
+; So we can use let*:
+(define (throw- v deg)
   (let* ((pi (* 4 (atan 1.0)))
-         (rad (* pi (/ a 180)))
+         (rad (* pi (/ deg 180))) 
          (vx (* v (cos rad)))
          (vy (* v (sin rad)))
          (t (/ (* 2 vy) 9.81)))
     (* vx t)))
 
-;
+; from the solution
 (define (throw v a)
   (let ((r (/ (* 4 a (atan 1.0)) 180)))
        (/ (* 2 v v (cos r) (sin r)) 9.81)))
